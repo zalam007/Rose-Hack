31a75e6d57154810b31b3ac788d486f0c46858b6
Zami Alam <zalam007@xe-02.cs.ucr.edu>
2024-01-20 15:04:53 -0800

    lots chang

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main() {
     // Get binary input from the user
     string binaryInput = getBinaryInput();
 
+
     // Convert binary to decimal and display the result
     int decimalResult = binaryToDecimal(binaryInput);
     cout << "Decimal equivalent: " << decimalResult << endl;
