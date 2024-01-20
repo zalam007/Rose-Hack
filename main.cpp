8ef6ab72c7f52afb25fb56dd1c91020e3d323786
Zami Alam <zalam007@ucr.edu>
2024-01-20 14:13:37 -0800

    files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
+#include "binary.h"
 #include <iostream>
+
 using namespace std;
-int main{
-    cout << "hi world:)" << endl;
-    //
+
+int main() {
+    cout << "test" << endl;
+    // string binaryInput;
+
+    // // Get binary input from the user
+    // cout << "Enter a binary number: ";
+    // cin >> binaryInput;
+
+    // // Validate binary input
+    // for (char digit : binaryInput) {
+    //     if (digit != '0' && digit != '1') {
+    //         cerr << "Invalid binary input. Please enter a binary number.\n";
+    //         return 1; // Exit with an error code
+    //     }
+    // }
+
+    // // Convert binary to decimal and display the result
+    // int decimalResult = binaryToDecimal(binaryInput);
+    // cout << "Decimal equivalent: " << decimalResult << endl;
+
     return 0;
 }
